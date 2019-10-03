9205fd217649b20cefdf932e68dac1ba624bc546
AgeuPaulo <uega123@gmail.com>
2019-10-02 21:54:55 -0300

    Prova primeira unidade

--- lib/data/helpers/helperdisciplina.dart
+++ lib/data/helpers/helperdisciplina.dart
@@ -1,5 +1,8 @@
 
 import 'package:magister_mobile/data/helpers/helperbase.dart';
+import 'package:magister_mobile/data/helpers/helpercurso.dart';
+import 'package:magister_mobile/data/helpers/helpermatricula.dart';
+import 'package:magister_mobile/data/helpers/helperturma.dart';
 import 'package:magister_mobile/data/models/disciplina.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -34,6 +37,19 @@ class HelperDisciplina extends HelperBase<Disciplina> {
         }
         return lista;
       });
+
+   Future<List> getAllDiscFromAluno(int idCursoFromAluno) async {
+    Database dbContact = await db;
+    List listMap = await dbContact.rawQuery("SELECT DISTINCT disc.${HelperDisciplina.idColumn}, disc.${HelperDisciplina.nomeColumn}, disc.${HelperDisciplina.creditosColumn}, disc.${HelperDisciplina.tipoColumn}, disc.${HelperDisciplina.hrsObgColumn}, disc.${HelperDisciplina.limiteColumn}, disc.${HelperDisciplina.idCursoColumn} FROM $disciplinaTable AS disc "                                 
+                                             "INNER JOIN ${HelperTurma.turmaTable} AS turma ON turma.${HelperTurma.idDiscColumn} = disc.${HelperDisciplina.idColumn} "
+                                             "INNER JOIN ${HelperMatricula.matriculaTable} AS mat ON mat.${HelperMatricula.turmaIdDiscColumn} = turma.${HelperTurma.idDiscColumn} "
+                                            "WHERE curso.${HelperMatricula.idAlunoColumn} = $idCursoFromAluno");                                 
+    List<Disciplina> listProf = List();
+    for (Map m in listMap) {
+      listProf.add(Disciplina.fromMap(m));
+    }
+    return listProf;
+  }
 
   @override
   Future<Disciplina> getFirst(int id) async => db.then((database) async {
